d585174f32b57a87183a4b7d56e594029b3f05c2
Nasir <momin96@gmail.com>
2018-04-10 15:44:13 +0530

    New Big nerd ranch project created named carlot

---
+++ NSRCarLot/NSRCarLot/Document.swift
@@ -0,0 +1,29 @@
+//
+//  Document.swift
+//  NSRCarLot
+//
+//  Created by Nasir Ahmed Momin on 10/04/18.
+//  Copyright © 2018 Nasir Ahmed Momin. All rights reserved.
+//
+
+import Cocoa
+
+class Document: NSPersistentDocument {
+
+    override init() {
+        super.init()
+        // Add your subclass-specific initialization here.
+    }
+
+    override class var autosavesInPlace: Bool {
+        return true
+    }
+
+    override func makeWindowControllers() {
+        // Returns the Storyboard that contains your Document window.
+        let storyboard = NSStoryboard(name: NSStoryboard.Name("Main"), bundle: nil)
+        let windowController = storyboard.instantiateController(withIdentifier: NSStoryboard.SceneIdentifier("Document Window Controller")) as! NSWindowController
+        self.addWindowController(windowController)
+    }
+
+}
---
+++ NSRCarLot/NSRCarLot/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NSRCarLot
+//
+//  Created by Nasir Ahmed Momin on 10/04/18.
+//  Copyright © 2018 Nasir Ahmed Momin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
