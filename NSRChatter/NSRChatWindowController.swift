f931f8e31879df57c67f58baab91f78db2dc5575
Nasir <momin96@gmail.com>
2018-04-16 16:40:26 +0530

    New chatter demo app started

---
+++ NSRChatter/NSRChatWindowController.swift
@@ -0,0 +1,18 @@
+//
+//  NSRChatWindowController.swift
+//  NSRChatter
+//
+//  Created by Nasir Ahmed Momin on 16/04/18.
+//  Copyright © 2018 Nasir Ahmed Momin. All rights reserved.
+//
+
+import Cocoa
+
+class NSRChatWindowController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
---
+++ NSRChatter/NSRChatter/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  NSRChatter
+//
+//  Created by Nasir Ahmed Momin on 16/04/18.
+//  Copyright © 2018 Nasir Ahmed Momin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    
+    @IBAction func newChatWindow(sender : NSButton) {
+        
+    }
+
+}
+
