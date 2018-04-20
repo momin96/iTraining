fd5178d1bada15af1f942ae6c9727345572411c9
Nasir <momin96@gmail.com>
2018-04-20 10:33:09 +0530

    New Ranch forcast demo project create

---
+++ RanchForcast/RanchForcast/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RanchForcast
+//
+//  Created by Nasir Ahmed Momin on 20/04/18.
+//  Copyright © 2018 Nasir Ahmed Momin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ RanchForcast/RanchForcast/NSRMainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  NSRMainViewController.swift
+//  RanchForcast
+//
+//  Created by Nasir Ahmed Momin on 20/04/18.
+//  Copyright © 2018 Nasir Ahmed Momin. All rights reserved.
+//
+
+import Cocoa
+
+class NSRMainViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
