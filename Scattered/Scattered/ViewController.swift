a8eab662249124ee05f2e037a8e8970a7d0eb52e
Nasir <momin96@gmail.com>
2018-04-24 18:16:05 +0530

    Setting up layer

--- Scattered/Scattered/ViewController.swift
+++ Scattered/Scattered/ViewController.swift
@@ -10,18 +10,49 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    var textLayer : CATextLayer!
+    
+    var text : String?
+    
+    func addImagesfromFolrderURL(_ folderURL: URL) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
+        view.layer = CALayer()
+        view.wantsLayer = true
+        
+        let textContainer = CALayer()
+        textContainer.anchorPoint = CGPoint.zero
+        textContainer.position = CGPoint(x: 10, y: 10)
+        textContainer.zPosition = 100
+        textContainer.backgroundColor = NSColor.black.cgColor
+        textContainer.borderColor = NSColor.white.cgColor
+        textContainer.borderWidth = 2
+        textContainer.cornerRadius = 15
+        textContainer.shadowOpacity = 0.5
+        view.layer?.addSublayer(textContainer)
+        
+        let textLayer = CATextLayer()
+        textLayer.anchorPoint = CGPoint.zero
+        textLayer.position = CGPoint(x: 10, y: 6)
+        textLayer.zPosition = 100
+        textLayer.fontSize = 24
+        textLayer.foregroundColor = NSColor.white.cgColor
+        self.textLayer = textLayer
+        
+        textContainer.addSublayer(textLayer)
+        
+        text = "Loading..."
+        
+        let picURL = URL(fileURLWithPath: "/Library/Desktop Pictures")
+        
+        addImagesfromFolrderURL(picURL)
+        
 
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
     }
-
 
 }
 
