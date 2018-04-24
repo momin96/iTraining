2b6195e5b30f574e3e465e397abf3e5f8a396658
Nasir <momin96@gmail.com>
2018-04-24 18:04:03 +0530

    New demo app created for scattered

---
+++ Scattered/Scattered/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Scattered
+//
+//  Created by Nasir Ahmed Momin on 24/04/18.
+//  Copyright © 2018 Nasir Ahmed Momin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Scattered/Scattered/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Scattered
+//
+//  Created by Nasir Ahmed Momin on 24/04/18.
+//  Copyright © 2018 Nasir Ahmed Momin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
