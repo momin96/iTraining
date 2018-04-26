e88ebdb5f565c418ead745fad7e5e64a48720c0e
Nasir <momin96@gmail.com>
2018-04-26 17:03:25 +0530

    some changes for ZIPspector app

--- ZIPSpector/ZIPSpector/Document.swift
+++ ZIPSpector/ZIPSpector/Document.swift
@@ -8,8 +8,13 @@
 
 import Cocoa
 
-class Document: NSDocument {
+class Document: NSDocument, NSTableViewDataSource {
 
+    
+    @IBOutlet weak var tableView : NSTableView!
+    
+    var filenames : [String] = []
+    
     override init() {
         super.init()
         // Add your subclass-specific initialization here.
@@ -18,6 +23,15 @@ class Document: NSDocument {
     override class var autosavesInPlace: Bool {
         return true
     }
+    
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return filenames.count
+    }
+    
+    func tableView(_ tableView: NSTableView, objectValueFor tableColumn: NSTableColumn?, row: Int) -> Any? {
+        return filenames[row]
+    }
+    
 
     override var windowNibName: NSNib.Name? {
         // Returns the nib file name of the document
@@ -31,6 +45,38 @@ class Document: NSDocument {
         throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
     }
 
+    override func read(from url: URL, ofType typeName: String) throws {
+        let filename = url.path
+        
+        let task = Process()
+        task.launchPath = "/usr/bin/zipinfo"
+        task.arguments = ["-1", filename]
+
+        let outPipe = Pipe()
+        task.standardOutput = outPipe
+        
+        task.launch()
+        
+        let fileHandle = outPipe.fileHandleForReading
+        let data = fileHandle.readDataToEndOfFile()
+        
+        task.waitUntilExit()
+        let status = task.terminationStatus
+        
+        if status != 0 {
+            let errorDomain = "com.bignerdranch.ProcessReturnCodeErrorDomain"
+            let errorInfo = [NSLocalizedFailureReasonErrorKey : "zipInfo return \(status)"]
+            throw NSError(domain: errorDomain, code: 0, userInfo: errorInfo)
+        }
+        
+        let stringName =  String.init(data: data, encoding: .utf8)
+        filenames = (stringName?.components(separatedBy: "\n"))! as [String]
+        
+        Swift.print("filenames \(filenames)")
+        
+        tableView.reloadData()
+    }
+    
     override func read(from data: Data, ofType typeName: String) throws {
         // Insert code here to read your document from the given data of the specified type. If outError != nil, ensure that you create and set an appropriate error when returning false.
         // You can also choose to override readFromFileWrapper:ofType:error: or readFromURL:ofType:error: instead.
