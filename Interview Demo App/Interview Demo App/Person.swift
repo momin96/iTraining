0eeae1e362ffe1ca1059141f16ca0578f2955581
Nasir <momin96@gmail.com>
2018-05-03 11:30:54 +0530

    New file added

---
+++ Interview Demo App/Interview Demo App/Person.swift
@@ -0,0 +1,14 @@
+//
+//  Person.swift
+//  Interview Demo App
+//
+//  Created by Nasir Ahmed Momin on 03/05/18.
+//  Copyright © 2018 Nasir Ahmed Momin. All rights reserved.
+//
+
+import Cocoa
+@objc(Person)
+class Person: NSObject {
+    @objc dynamic var firstName : String = "Nasir Ahmed"
+    @objc dynamic var lastName : String = "Momin"
+}
