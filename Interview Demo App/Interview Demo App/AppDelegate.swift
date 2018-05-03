21156b710e58d422ca32c0b9b638d3eed149124e
Nasir <momin96@gmail.com>
2018-05-03 10:10:09 +0530

    New demo app create

---
+++ Interview Demo App/Interview Demo App/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Interview Demo App
+//
+//  Created by Nasir Ahmed Momin on 03/05/18.
+//  Copyright © 2018 Nasir Ahmed Momin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Interview Demo App/Interview Demo App/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Interview Demo App
+//
+//  Created by Nasir Ahmed Momin on 03/05/18.
+//  Copyright © 2018 Nasir Ahmed Momin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
