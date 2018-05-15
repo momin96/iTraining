b9e074e1c4b76fd3dbb92b7033d14f2fb99ad043
Nasir <momin96@gmail.com>
2018-05-15 12:25:51 +0530

    new project added

---
+++ NSR Logger/NSR Logger/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NSR Logger
+//
+//  Created by Nasir Ahmed Momin on 15/05/18.
+//  Copyright © 2018 Nasir Ahmed Momin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
