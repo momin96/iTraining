b0634b24e53b866bdf3e6ffa7a200f1ed6ed3e07
Nasir Ahmed Momin <nasir.am@exilant.com>
2018-06-08 15:20:46 +0530

    Some documentation

--- Internet Connect Demo/Internet Connect Demo/NetworkManager.swift
+++ Internet Connect Demo/Internet Connect Demo/NetworkManager.swift
@@ -17,11 +17,14 @@ enum ConnectionType {
 
 class NetworkManager {
     
-    static let shared = NetworkManager()
+    // Open to any one who whats to use
+    open static let shared = NetworkManager()
     
+    //Private properties
     private var reachability: Reachability?
     private let hostName = "www.google.com"
     
+    // MARK: Constructor & destructor
     init() {
         print("init")
         self.configNetwork()
@@ -31,58 +34,8 @@ class NetworkManager {
         unregisterReachabilityObserver()
     }
     
-    private func configNetwork () {
-        print("configNetwork")
-        reachability = Reachability(hostname: hostName)
-        registerReachablityObserver()
-    }
-    
-    private func registerReachablityObserver () {
-        print(" Register ")
-        NotificationCenter.default.addObserver(
-            self,
-            selector: #selector(reachabilityChangedNotification(_:)),
-            name: .reachabilityChanged,
-            object: reachability
-        )
-    }
-    
-    private func unregisterReachabilityObserver () {
-        print(" Unregister ")
-
-        NotificationCenter.default.removeObserver(self,
-                                                  name: .reachabilityChanged,
-                                                  object: nil)
-    }
-    
-    private func startNotifier() {
-        print("--- start notifier")
-        do {
-            try reachability?.startNotifier()
-        } catch {
-            print("Issue in startNotifier")
-            return
-        }
-    }
-    
-    @objc private func reachabilityChangedNotification(_ note : Notification) {
-    
-        let reachability = note.object as! Reachability
-        
-        if reachability.connection != .none {
-            print(" network reachable ")
-        } else {
-            print(" network un reachable ")
-        }
-    }
-    
-    private func stopNotifier() {
-        print("--- stop notifier")
-        reachability?.stopNotifier()
-        NotificationCenter.default.removeObserver(self, name: .reachabilityChanged, object: nil)
-        reachability = nil
-    }
-    
+  
+    // MARK: Public Methods
     public func isNetworkAvailable () -> Bool {
         
         if reachability?.connection == .wifi {
@@ -121,4 +74,59 @@ class NetworkManager {
 
         return .typeNone
     }
+    
+    
+    // MARK: Private functions
+    private func configNetwork () {
+        print("configNetwork")
+        reachability = Reachability(hostname: hostName)
+        registerReachablityObserver()
+    }
+    
+    private func registerReachablityObserver () {
+        print(" Register ")
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(reachabilityChangedNotification(_:)),
+            name: .reachabilityChanged,
+            object: reachability
+        )
+    }
+    
+    private func unregisterReachabilityObserver () {
+        print(" Unregister ")
+        
+        NotificationCenter.default.removeObserver(self,
+                                                  name: .reachabilityChanged,
+                                                  object: nil)
+    }
+    
+    private func startNotifier() {
+        print("--- start notifier")
+        do {
+            try reachability?.startNotifier()
+        } catch {
+            print("Issue in startNotifier")
+            return
+        }
+    }
+    
+    @objc private func reachabilityChangedNotification(_ note : Notification) {
+        
+        let reachability = note.object as! Reachability
+        
+        if reachability.connection != .none {
+            print(" network reachable ")
+        } else {
+            print(" network un reachable ")
+        }
+    }
+    
+    private func stopNotifier() {
+        print("--- stop notifier")
+        reachability?.stopNotifier()
+        NotificationCenter.default.removeObserver(self, name: .reachabilityChanged, object: nil)
+        reachability = nil
+    }
+    
 }
