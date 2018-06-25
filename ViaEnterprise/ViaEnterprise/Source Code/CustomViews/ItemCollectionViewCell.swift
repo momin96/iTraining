075a448f05c221ff7a56800977fedc1c0783f988
Nasir Ahmed Momin <nasir.am@exilant.com>
2018-06-25 12:27:54 +0530

    IB Connection

--- ViaEnterprise/ViaEnterprise/Source Code/CustomViews/ItemCollectionViewCell.swift
+++ ViaEnterprise/ViaEnterprise/Source Code/CustomViews/ItemCollectionViewCell.swift
@@ -10,9 +10,30 @@ import UIKit
 
 class ItemCollectionViewCell: UICollectionViewCell {
 
+    @IBOutlet weak var containerView: UIView!
+    
+    @IBOutlet weak var itemImageView: UIImageView!
+    @IBOutlet weak var itemName: UILabel!
+    
+    @IBOutlet weak var itemPackQty: UILabel!
+    @IBOutlet weak var itemPrice: UILabel!
+    
+    @IBOutlet weak var itemTotalQty: UILabel!
+    @IBOutlet weak var itemIncreamentButton: UIButton!
+    @IBOutlet weak var itemDecrementButton: UIButton!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
 
     }
-
+    
+    
+    @IBAction func decrementItem(_ sender: UIButton) {
+    
+    }
+    
+    @IBAction func incrementItem(_ sender: UIButton) {
+    
+    }
+    
 }
